b0eed3528ea6ec8f736fb34374f4170df0daf40a
6a6n1k <32800626+6a6n1k@users.noreply.github.com>
2020-01-07 22:40:02 +0500

    Update pars.py

--- pars.py
+++ pars.py
@@ -1,20 +1,63 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import re
+import telebot;
 
-def get_count():
-    url = "https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=59.09,52.64,-58.77,-47.71&faa=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=7200&gliders=1&stats=1"
+bot = telebot.TeleBot('937403692:AAGeiMrpb5N-zgN1rLCUHlGLV93aR4kUyhA');
 
-    # Request with ou will get an 403 HTTP error
-    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+url=('https://www.flightradar24.com/')
+headers = {'accept': '*/*',
+           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36 '}
+base_url = 'https://ru.flightaware.com/ajax/trackpoll.rvt?token=88dd7c1a0d41355dafa2ce4ff0e607704b11c422c13281779a15400a02823f7ccf96a80adf3baec3--76e86d4bff8ac1c5d7a0813749e14e00972848c7&locale=ru_RU&summary=0'
 
-    # Parse the JSON
-    data = r.json()
-    counter = 0
 
-    # Iterate over the elements to get the number of total flights
-    for element in data["stats"]["total"]:
-        counter += data["stats"]["total"][element]
 
-    return counter
+def fr_parse(base_url, headers):
+    session = requests.Session()
+    request = session.get(base_url, headers=headers)
+    if request.status_code == 200:
+        soup = bs(request.content, 'html.parser').text
+        #reis
+        reis=re.findall(r'"displayIdent":"\w+',soup)
+        reisstr1=','.join(reis)
+        reis1=re.sub(r'"displayIdent":"',"",reisstr1)
+        reis2=reis1.split(',')
 
-while True:
-    print(get_count())
-    time.sleep(8)
+        #bort
+        bort=re.findall(r'"gate":null,"terminal":null,"delays":null},"type":"\w+',soup)
+        bortstr1=','.join(bort)
+        bort1=re.sub(r'"gate":null,"terminal":null,"delays":null},"type":"',"",bortstr1)
+        bort2=bort1.split(',')
+
+        #from-to
+        ft=re.findall(r'"isValidAirportCode":true,"altIdent":"\w+',soup)
+        ftstr1=','.join(ft)
+        ft1=re.sub(r'"isValidAirportCode":true,"altIdent":"',"",ftstr1)
+        ft2=ft1.split(',')
+        ft3=(bort2)[0:len(bort2)]
+        for i in range(int((len(ft2))/2)):
+            a=i
+            b=i+1
+            ft3[i]=ft2[i+b]+' => '+ft2[i+a]
+
+        urlg=reis2
+        for i in range(len(reis2)):
+            urlg[i]=(str(i+1))+') '+url+reis2[i]+'  '+bort2[i]+'  '+ft3[i]
+        return(urlg)
+#
+
+    else:
+        print('Error')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Tas":
+        urlg = fr_parse(base_url, headers)
+        str = '\n'.join(urlg)
+        bot.send_message(message.from_user.id, str)
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши Tas")
+    else:
+        bot.send_message(message.from_user.id, "Что бы увидеть выполняемые рейсы в/из Ташкента отправь Tas")
+
+bot.polling(none_stop=True, interval=0)
